e7dac2513d2bcdf010cbd41f0a6caf7526c55f1d
Code4Hacker <black2022mail@gmail.com>
2024-03-18 19:09:50 +0300

    actual_data_begin

--- src/components/BarTop.jsx
+++ src/components/BarTop.jsx
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react'
 import { udom_logo } from '../assets'
-import { Row } from 'react-bootstrap'
-import { ArrowDown, Fullscreen, Toggle2On } from 'react-bootstrap-icons'
+import { Button, Row } from 'react-bootstrap'
+import { ArrowDown, BagCheckFill, BoxArrowRight, Fullscreen, Toggle2On } from 'react-bootstrap-icons'
 import { useNavigate } from 'react-router-dom'
 import { useIdleTimer } from 'react-idle-timer'
+import { OverlayPanel } from 'primereact/overlaypanel';
 
 const BarTop = ({ username, menu, extras }) => {
+    const op = useRef(null);
     const tableRef = useRef(null);
     const navigator = useNavigate();
 
@@ -41,6 +43,37 @@ const BarTop = ({ username, menu, extras }) => {
                                 <i><Toggle2On /></i>
                             </button>
                         </div>
+                        <OverlayPanel ref={op} className=''>
+                            <div className="divide-y divide-gray-100 shadow-xl rounded" style={{
+                                backgroundColor:'var(--light)'
+                            }}>
+                                    
+                                    <div className="hidden shrink-0 divide-gray-100 devide sm:flex sm:flex-col sm:items-end rounded">
+                                        <Button style={{
+                                            border:"none",
+                                            color:"var(--muted)",
+                                            width:"100%",
+                                            display:"flex",
+                                            gap:"10px",
+                                            background:"none !important",
+                                            fontSize:"medium",
+                                            borderBottom:"1px solid  var(--meal)",
+                                            borderRadius:"0px"
+                                        }} className='p-3 hover:text-white hover:bg-black hover:rounded'><BagCheckFill className='hover:text-white'/> <span className='-mt-1 hover:text-white'>Change  Password</span></Button>
+                                        <Button style={{
+                                            border:"none",
+                                            color:"var(--muted)",
+                                            width:"100%",
+                                            display:"flex",
+                                            gap:"10px",
+                                            fontSize:"medium",
+                                            borderBottom:"1px solid  var(--meal)",
+                                            borderRadius:"0px"
+                                        }}className='p-3 hover:bg-black hover:rounded'><BoxArrowRight className='hover:text-white'/> <span className='-mt-1 hover:text-white'> Log  Out</span></Button>
+                                    </div>
+                            </div>
+                            
+                        </OverlayPanel>
                         <div className="border_end">
                             <button className='primary'>
                                 <span className="icon-b" style={{
@@ -51,7 +84,7 @@ const BarTop = ({ username, menu, extras }) => {
                                 <div className="image">
                                     <img src={udom_logo} alt="" />
                                 </div>
-                                <div className="">
+                                <div onClick={(e) => op.current.toggle(e)}>
                                     <span>Paulo(Student)</span>
                                 </div>
                                 <span className='icon-bg'><ArrowDown /></span>
