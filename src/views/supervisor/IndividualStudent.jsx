c90a47e74a0455c3c733a195ca454f1e4a968252
Code4Hacker <black2022mail@gmail.com>
2024-03-28 15:59:56 +0300

    location

--- src/views/supervisor/IndividualStudent.jsx
+++ src/views/supervisor/IndividualStudent.jsx
@@ -4,26 +4,65 @@ import { BarTop, Sidebar, Topbar } from '../../components'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import { baseURL } from '../../paths/base_url';
+import toast from 'react-hot-toast';
 
 
 const IndividualStudent = () => {
     const [student, setStudent] = useState({});
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
+    const [nameloc, setNameloc] = useState();
+
     const params = useParams();
-    const {id} = params;
-    const handleGetUser =  async() => {
+    const { id } = params;
+    const handleGetUser = async () => {
         let formdata = new FormData();
         formdata.append("studentId", id);
         const bodydata = formdata;
         const requests = axios.request({
-            url:`${baseURL}arrival_data.php`,
-            method:"POST",
+            url: `${baseURL}arrival_data.php`,
+            method: "POST",
             data: bodydata
         });
 
-        console.log((await requests).data.arrival[0]);
+        // console.log((await requests).data.arrival[0]);
+        (await requests).data.arrival[0]?getUserCoordinates():toast.error("something happed");
         setStudent((await requests).data.arrival[0]);
     }
-    useEffect(()  => {
+
+    const geolocationAPI = navigator.geolocation;
+    const getUserCoordinates = () => {
+        if (!geolocationAPI) {
+            console.log("Geolocation API is not available in your browser!");
+        } else {
+            geolocationAPI.getCurrentPosition(
+                async (position) => {
+                    const { coords } = position;
+                    // setLat(coords.latitude);
+                    // setLong(coords.longitude);
+                    // // codeLatLng(coords.latitude, coords.longitude);
+                    // alert(coords.longitude);
+
+                    try {
+                        const request_for = axios.request({
+                            url:`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`,
+                            method:"GET"
+                        });
+                        setNameloc((await request_for).data);
+                    } catch (error) {
+                        toast.error(error);
+                    }
+
+                },
+                (error) => {
+                    console.log("Something went wrong getting your position!");
+                }
+            );
+            console.log("");
+        }
+    };
+    setTimeout(handleGetUser,10000);
+    useEffect(() => {
         handleGetUser();
     }, []);
     return (
@@ -37,14 +76,14 @@ const IndividualStudent = () => {
                 <div className="right-screen-view">
                     <BarTop />
                     <Topbar
-                        headline={"Welcome to Academic Year"}
+                        headline={`${student !== null ? student.region + " " + student.district + " " + student.place : "udom, cive"}`}
                         subheadline={"Dashboard"}
-                        note={"2022/2023"}
+                        note={""}
                     />
                     <div>
                         {/* IndividualStudent {params.id} */}
                         <div className="absolute">
-                            <div><div><iframe className='border_box fullbox'  src={`https://www.google.com/maps/embed/v1/directions?origin=Dodoma,+Tanzania&destination=${student !== null ? student.region+" "+student.district+" "+student.place:"udom, cive"}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}></iframe></div></div>
+                            <div><div><iframe className='border_box fullbox' src={`https://www.google.com/maps/embed/v1/directions?origin=${nameloc !== undefined ? nameloc.city +" "+ nameloc.locality+" "+ nameloc.countryName: "udom"}&destination=${student !== null ? student.region + " " + student.district + " " + student.place : "udom, cive"}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}></iframe></div></div>
                         </div>
                     </div>
                 </div>
